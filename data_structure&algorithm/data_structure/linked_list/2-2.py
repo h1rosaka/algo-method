8d36706f2d6889e31ed534a32e44bfdc7f4f6204
hiroakisakamoto <hsakamoto111@gmail.com>
2024-04-12 14:53:17 +0900

    Good Job

---
+++ data_structure&algorithm/data_structure/linked_list/2-2.py
@@ -0,0 +1,37 @@
+class Node():
+    def __init__(self, v=""):
+        self.value = v
+        self.nex = None
+
+nil = Node()
+nil.nex = nil
+
+num_queries = int(input())
+
+def push_heads(s):
+    new_node = Node(s)
+    new_node.nex = nil.nex
+    nil.nex = new_node 
+
+def pop_head():
+    front = nil.nex
+    if front == nil:
+        print('Error')
+    else:
+        print(front.value)
+        nil.nex = nil.nex.nex
+        del front
+
+for _ in range(num_queries):
+    query = input().split()
+
+    if query[0]=='0':
+        S = query[1]
+        push_heads(S)
+    else:
+        pop_head()
+
+
+
+
+
