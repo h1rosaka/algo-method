8bb9e7eb6adb714cbf50f2717f4d3e099ea3278b
hiroakisakamoto <hsakamoto111@gmail.com>
2024-04-30 10:22:16 +0900

    Good Job

---
+++ data_structure&algorithm/data_structure/hash_table_and_associateve_array/1-1.py
@@ -0,0 +1,32 @@
+
+# TLE
+N = int(input())
+S = list(input().split())
+Q = int(input())
+
+def hash30(origninal_string):
+    hashed_value_of_the_string = 0
+
+    for i in range(len(origninal_string)):
+        reverse_ith_index = -(1+i) # 後ろからi番目
+        character = origninal_string[reverse_ith_index]
+        n = ord(character) - ord("a") + 1
+        hashed_value_of_the_string += n * (30**i)
+
+    return hashed_value_of_the_string
+
+maxval = 30**5 -1
+
+# カウンタバケットを埋める
+counter = [0 for _ in range(maxval)]
+for s in S:
+    hashed_s = hash30(s)
+    counter[hashed_s] += 1
+
+for _ in range(Q):
+    T = input()
+    hashed_T = hash30(T)
+    print(counter[hashed_T])
+
+
+
