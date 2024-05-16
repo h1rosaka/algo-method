e7d3c0c86a1f36d97f8ec9c748558b7e1049264c
hiroakisakamoto <hsakamoto111@gmail.com>
2024-05-16 23:00:25 +0900

    Good Job

---
+++ data_structure&algorithm/data_structure/hash_table_and_associateve_array/10-1.py
@@ -0,0 +1,21 @@
+# TLE
+X = int(input())
+Q = int(input())
+
+
+
+completed_time = []
+
+for _ in range(Q):
+    query = list(map(int, input().split()))
+  
+    t = query[1]
+    if query[0] == 0:
+        completed_time.append(t+X)
+    else:
+        finished_tasks = [x for x in completed_time if x <= t] # こんなんできるっけ　＆filter
+
+        print(len(finished_tasks))
+
+
+    
