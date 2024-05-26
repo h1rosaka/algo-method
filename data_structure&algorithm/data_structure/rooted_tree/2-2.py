c581bf0569a6d1d1a2ce2026c18e7b139d5ce98e
hiroakisakamoto <hsakamoto111@gmail.com>
2024-05-26 11:02:13 +0900

    Good Job

---
+++ data_structure&algorithm/data_structure/rooted_tree/2-2.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+
+parent = [-1 for _ in range(N)]
+children = [[] for _ in range(N)]
+
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    parent[b] = a
+    children[a].append(b)
+
+
+Q = int(input())
+
+for _ in range(Q):
+    v = int(input())
+
+    parent_v = parent[v]
+
+    print(*sorted(children[parent_v]))
